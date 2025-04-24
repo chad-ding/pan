42021e2230cdc25da956ebe4f9b1555450910ed3
丁铭锋 <11100798@vivo.com>
2025-04-24 15:32:02 +0800

    refactor: 支持图片转base64

--- popup/app.vue
+++ popup/app.vue
@@ -1,20 +1,8 @@
 <template>
 	<div class="app">
 		<el-tabs v-model="currenTab">
-			<el-tab-pane name="qrcode" label="二维码">
-				<qr-code />
-			</el-tab-pane>
-			<el-tab-pane name="codec" label="字符串编/解码">
-				<codec />
-			</el-tab-pane>
-			<el-tab-pane name="color" label="色值转换">
-				<color />
-			</el-tab-pane>
-			<el-tab-pane name="json" label="JSON格式化">
-				<json />
-			</el-tab-pane>
-			<el-tab-pane name="numeric" label="数字转化">
-				<numeric />
+			<el-tab-pane v-for="(value, key) of tabs" :key="key" :name="key" :label="value.label">
+				<component :is="value.component" />
 			</el-tab-pane>
 		</el-tabs>
 	</div>
@@ -26,17 +14,39 @@ import Codec from '@/view/codec/index.vue'
 import Color from '@/view/color/index.vue'
 import Json from '@/view/json/index.vue'
 import Numeric from '@/view/numeric/index.vue'
+import Pic2Base64 from '@/view/pic2base64/index.vue'
 
 export default {
-	components: {
-		QrCode,
-		Codec,
-		Color,
-		Json,
-		Numeric
-	},
 	data() {
+		const tabs = {
+			qrcode: {
+				label: '二维码',
+				component: QrCode
+			},
+			codec: {
+				label: '字符串编/解码',
+				component: Codec
+			},
+			color: {
+				label: '色值转换',
+				component: Color
+			},
+			json: {
+				label: 'JSON格式化',
+				component: Json
+			},
+			pic2base64: {
+				label: '图片转base64',
+				component: Pic2Base64
+			},
+			numeric: {
+				label: '数字转化',
+				component: Numeric
+			}
+		}
+
 		return {
+			tabs,
 			currenTab: 'qrcode'
 		}
 	}
