ccb204df024911033ba7195e259d4c694b32bbdf
丁铭锋 <11100798@vivo.com>
2025-04-28 20:42:36 +0800

    refactor: 拆分打包脚本

--- build/backend.js
+++ build/backend.js
@@ -4,17 +4,17 @@ const exec = require('child_process').exec
 const fse = require('fs-extra')
 const chalk = require('chalk')
 
-const manifest = require('./manifest.json')
-const pkg = require('./package.json')
-
-const dist = path.relative(__dirname, './dist')
-fse.ensureDirSync(dist)
+const pkg = require('../package.json')
+const manifest = require('../manifest.json')
 manifest.version = pkg.version
+
+const dist = path.relative(__dirname, '../dist')
+fse.ensureDirSync(dist)
 
 const watching = process.argv[2] === '--watch'
 
 fs.writeFile(
-	path.resolve(dist, './manifest.json'),
+	path.resolve(dist, '../manifest.json'),
 	JSON.stringify(manifest, null, 4),
 	{ encoding: 'utf-8' },
 	err => {
@@ -27,7 +27,7 @@ fs.writeFile(
 	}
 )
 
-fs.cp(path.resolve(__dirname, './icon'), path.join(dist, './icon'), { recursive: true }, err => {
+fs.cp(path.resolve(__dirname, '../icon'), path.join(dist, '../icon'), { recursive: true }, err => {
 	if (err) {
 		console.log(chalk.red('复制icon失败'))
 		console.log(chalk.red(err))
@@ -36,26 +36,17 @@ fs.cp(path.resolve(__dirname, './icon'), path.join(dist, './icon'), { recursive:
 	}
 })
 
-exec('npx rollup -c rollup.config.js', (err, stdout) => {
+let command = 'npx rollup -c rollup.config.js'
+if (watching) {
+	command = 'npx rollup -c --watch rollup.config.js'
+}
+
+exec(command, (err, stdout) => {
 	console.log(chalk.green(stdout))
 	if (err) {
 		console.log(chalk.red('编译content、background脚本失败'))
 		console.log(chalk.red(err))
 	} else {
 		console.log(chalk.green('编译content、background脚本成功'))
-	}
-})
-
-let command = 'npx vite build'
-if (watching) {
-	command += ' --watch'
-}
-exec(command, (err, stdout) => {
-	console.log(chalk.green(stdout))
-	if (err) {
-		console.log(chalk.red('编译popup失败'))
-		console.log(chalk.red(err))
-	} else {
-		console.log(chalk.green('编译popup成功'))
 	}
 })
---
+++ build/frontend.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const exec = require('child_process').exec
+const fse = require('fs-extra')
+const chalk = require('chalk')
+
+const dist = path.relative(__dirname, '../dist')
+fse.ensureDirSync(dist)
+
+const watching = process.argv[2] === '--watch'
+
+console.log('>>>>>>>>> ', watching)
+
+let command = 'npx vite build'
+if (watching) {
+	command += ' --watch'
+}
+exec(command, (err, stdout) => {
+	console.log(chalk.green(stdout))
+	if (err) {
+		console.log(chalk.red('编译popup失败'))
+		console.log(chalk.red(err))
+	} else {
+		console.log(chalk.green('编译popup成功'))
+	}
+})
